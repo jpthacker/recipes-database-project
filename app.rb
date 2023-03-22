f911da59d2e2bd3fbe45d6f97fd9dc4dfe4354ab
Jack Thacker <jackthacker89@gmail.com>
2023-03-22 13:41:23 +0000

    Add print functionaility to app

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/recipe_repository'
 
 # We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+DatabaseConnection.connect('recipes_directory')
+
+recipe_repo = RecipeRepository.new
+recipes = recipe_repo.all
+
+recipes.each do |recipe|
+    puts "Recipe: #{recipe.recipe_name}, cooking time: #{recipe.cooking_time}, rating: #{recipe.rating}"
+end
